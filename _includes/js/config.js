b25b3a6c3f9f73393113241aca071c3d8a4e3ad7
Jörg Holetschek <j.holetschek@bgbm.org>
2021-04-16 12:13:05 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -5,5 +5,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 var siteConfig = {
   rootPredicate: { type: 'equals', key: 'networkKey', value: '3aee7756-565e-4dc5-b22c-f997fbd7105c' },
   occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'], // define which tabs to show and in which order
-  blacklistedFilters: ['basisOfRecord', 'depth', 'establishmentMeans', 'eventId', 'hostKey']
+  blacklistedFilters: ['basisOfRecord', 'depth', 'establishmentMeans', 'eventId', 'hostKey', 'identifiedById', 'mediaTypes', 'occurrenceStatus', 'organismId', 'organismQuantity', 'protocol', 'publishingCountryCode', 'recordedById', 'relativeOrganismQuantity', 'sampleSizeUnit', 'sampleSizeValue', 'samplingProtocol']
 };
