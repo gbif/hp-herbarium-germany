e3fe522b2416259052c43a93e6d6c5cdff258815
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2021-04-16 09:45:27 +0200

    add dataset tab

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,6 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'networkKey', value: '3aee7756-565e-4dc5-b22c-f997fbd7105c' }
+  rootPredicate: { type: 'equals', key: 'networkKey', value: '3aee7756-565e-4dc5-b22c-f997fbd7105c' },
+  occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // define which tabs to show and in which order
 };
