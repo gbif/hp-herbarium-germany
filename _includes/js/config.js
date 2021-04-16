a8e3d3b55092bb5c34a578b3a4910eb2fd951f47
Jörg Holetschek <j.holetschek@bgbm.org>
2021-04-16 11:59:57 +0200

    Filter blacklist added

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -5,4 +5,6 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 var siteConfig = {
   rootPredicate: { type: 'equals', key: 'networkKey', value: '3aee7756-565e-4dc5-b22c-f997fbd7105c' },
   occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // define which tabs to show and in which order
+  blacklistedFilters: ['basisOfRecord', 'depth', 'establishmentMeans', 'eventId', 'hostKey'],
+  highlightedFilters: ['collectionCode', 'taxonKey']
 };
