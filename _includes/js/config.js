8c5c370f3af1697e7e3c5fc13a5452ef97011d48
Morten Høfft <mhoefft@gbif.org>
2021-05-20 14:29:59 +0200

    Update filter name to align with API v1

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -6,5 +6,5 @@ var siteConfig = {
   rootPredicate: { type: 'equals', key: 'networkKey', value: '3aee7756-565e-4dc5-b22c-f997fbd7105c' },
   occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'], // define which tabs to show and in which order
   excludedFilters: ['basisOfRecord', 'depth', 'establishmentMeans', 'eventId', 'hostKey', 'identifiedById', 'mediaTypes', 'occurrenceStatus', 'organismId', 'organismQuantity', 'protocol', 'publishingCountryCode', 'recordedById', 'relativeOrganismQuantity', 'sampleSizeUnit', 'sampleSizeValue', 'samplingProtocol'],
-  highlightedFilters: ['q', 'taxonKey', 'typeStatus', 'recordedBy', 'locality', 'countryCode',  'year']
+  highlightedFilters: ['q', 'taxonKey', 'typeStatus', 'recordedBy', 'locality', 'country',  'year']
 };
