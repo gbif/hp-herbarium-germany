81f87b15c7c5dfa2a6fe5ffe7726380cad0d63e0
Jörg Holetschek <j.holetschek@bgbm.org>
2021-12-07 17:02:54 +0100

    Table columns changed

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -7,6 +7,6 @@ var siteConfig = {
   occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'], // define which tabs to show and in which order
   excludedFilters: ['continent', 'basisOfRecord', 'depth', 'establishmentMeans', 'eventId', 'hostingOrganizationKey', 'identifiedById', 'occurrenceStatus', 'organismId', 'organismQuantity', 'protocol', 'publishingCountryCode', 'recordedById', 'relativeOrganismQuantity', 'sampleSizeUnit', 'sampleSizeValue', 'samplingProtocol', 'dwcaExtension'],
   highlightedFilters: ['q', 'taxonKey', 'typeStatus', 'recordNumber', 'recordedBy', 'locality', 'country',  'year'],
-  defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'recordedBy', 'catalogNumber', 'dataset', 'publisher']
+  defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'recordedBy', 'dataset', 'publisher', 'catalogNumber']
   // defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber', 'recordedBy', 'identifiedBy']
 };
