82c756d96fa2d9d1776ef783a6f9a01aa7ae21d3
Jörg Holetschek <j.holetschek@bgbm.org>
2021-12-07 16:47:44 +0100

    Table columns setting added: All columns

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -6,5 +6,6 @@ var siteConfig = {
   rootPredicate: { type: 'equals', key: 'networkKey', value: '3aee7756-565e-4dc5-b22c-f997fbd7105c' },
   occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'], // define which tabs to show and in which order
   excludedFilters: ['continent', 'basisOfRecord', 'depth', 'establishmentMeans', 'eventId', 'hostingOrganizationKey', 'identifiedById', 'occurrenceStatus', 'organismId', 'organismQuantity', 'protocol', 'publishingCountryCode', 'recordedById', 'relativeOrganismQuantity', 'sampleSizeUnit', 'sampleSizeValue', 'samplingProtocol', 'dwcaExtension'],
-  highlightedFilters: ['q', 'taxonKey', 'typeStatus', 'recordNumber', 'recordedBy', 'locality', 'country',  'year']
+  highlightedFilters: ['q', 'taxonKey', 'typeStatus', 'recordNumber', 'recordedBy', 'locality', 'country',  'year'],
+  defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber', 'recordedBy', 'identifiedBy']
 };
