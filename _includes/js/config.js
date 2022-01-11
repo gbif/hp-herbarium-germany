2416f34b37c683d3ffaaae790d584e985354815a
Jörg Holetschek <j.holetschek@bgbm.org>
2022-01-11 12:59:29 +0100

    Filter order changed

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -6,7 +6,7 @@ var siteConfig = {
   rootPredicate: { type: 'equals', key: 'networkKey', value: '3aee7756-565e-4dc5-b22c-f997fbd7105c' },
   occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'], // define which tabs to show and in which order
   excludedFilters: ['continent', 'basisOfRecord', 'depth', 'establishmentMeans', 'eventId', 'hostingOrganizationKey', 'identifiedById', 'occurrenceStatus', 'organismId', 'organismQuantity', 'protocol', 'publishingCountryCode', 'recordedById', 'relativeOrganismQuantity', 'sampleSizeUnit', 'sampleSizeValue', 'samplingProtocol', 'dwcaExtension'],
-  highlightedFilters: ['q', 'taxonKey', 'typeStatus', 'recordNumber', 'recordedBy', 'locality', 'country',  'year'],
+  highlightedFilters: ['q', 'taxonKey', 'typeStatus', 'recordedBy', 'recordNumber', 'locality', 'country',  'year'],
   defaultTableColumns: ['features', 'country', 'year', 'recordedBy', 'dataset', 'publisher', 'catalogNumber']
   // defaultTableColumns: ['features', 'typeStatus', 'country', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber', 'recordedBy', 'recordNumber', 'identifiedBy']
 };
