5b566969becca2d1944a0d43cc88ca02825ee570
Jörg Holetschek <j.holetschek@bgbm.org>
2026-04-17 14:37:58 +0200

    Revert "Matomo tracking link updated"
    This reverts commit 4f92baba47908b417a6584389d1bc4e64545e4bc.

--- _includes/js/measure.js
+++ _includes/js/measure.js
@@ -5,9 +5,9 @@ function attachMeasurements() {
 	_paq.push(['trackPageView']); 
 	_paq.push(['enableLinkTracking']); 
 	(function() { 
-		var u="https://stats.bo.berlin/"; 
+		var u="https://ww3.bgbm.org/php/piwik3/"; 
 		_paq.push(['setTrackerUrl', u+'matomo.php']); 
-		_paq.push(['setSiteId', '38']); 
+		_paq.push(['setSiteId', '14']); 
 		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; 
 		g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s); 
 	  })(); 
