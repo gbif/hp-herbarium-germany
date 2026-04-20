09809ca7394890c1222fb55f47d442a698ab0609
Jörg Holetschek <j.holetschek@bgbm.org>
2026-04-20 11:49:36 +0200

    Next try: Matomo link updated

--- _includes/js/measure.js
+++ _includes/js/measure.js
@@ -7,7 +7,7 @@ function attachMeasurements() {
 	(function() { 
 		var u="https://stats.bo.berlin/"; 
 		_paq.push(['setTrackerUrl', u+'matomo.php']); 
-		_paq.push(['setSiteId', '38']); 
+		_paq.push(['setSiteId', '40']); 
 		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; 
 		g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s); 
 	  })(); 
